50d1527b16be71d9ff303e0253379be487d2227f
manestep <manestep@gmail.com>
2018-07-22 15:46:55 +0400

    .

--- ConsoleApp2/clientik.cs
+++ ConsoleApp2/clientik.cs
@@ -22,7 +22,7 @@ namespace ConsoleApp2
 
 
             //   var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("admin888", "administrator11", "UserAPI");
-            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("example11", "erkinq", "UserAPI");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("example11", "erkinq", "CatalogAPI");
 
 
             if (tokenResponse.IsError)
