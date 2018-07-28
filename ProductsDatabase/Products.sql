c4b969f661bf37ce9cce102a399130833bb96fc5
manestep <manestep@gmail.com>
2018-07-28 16:53:22 +0400

    added sp for most searched product

--- ProductsDatabase/Products.sql
+++ ProductsDatabase/Products.sql
@@ -1,19 +1,24 @@
 ﻿CREATE TABLE [dbo].[Products] (
-    [Id]         INT           IDENTITY (1, 1) NOT NULL,
-    [Name]       VARCHAR (30)  NOT NULL,
-    [Brand]      VARCHAR (20)  NOT NULL,
-    [Version]    VARCHAR (20)  NULL,
-    [Price]      MONEY         NOT NULL,
-    [RAM]        INT           NOT NULL,
-    [Year]       INT           NOT NULL,
-    [Display]    DECIMAL (18)  NULL,
-    [Battery]    INT           NULL,
-    [Camera]     INT           NOT NULL,
-    [Image]      VARCHAR (200) NOT NULL,
-    [Quantity]   INT           NULL,
-    [Catalog_Id] INT           NULL,
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        VARCHAR (30)  NOT NULL,
+    [Brand]       VARCHAR (20)  NOT NULL,
+    [Version]     VARCHAR (20)  NULL,
+    [Price]       MONEY         NOT NULL,
+    [RAM]         INT           NOT NULL,
+    [Year]        INT           NOT NULL,
+    [Display]     DECIMAL (18)  NULL,
+    [Battery]     INT           NULL,
+    [Camera]      INT           NOT NULL,
+    [Image]       VARCHAR (200) NOT NULL,
+    [Quantity]    INT           NULL,
+    [Catalog_Id]  INT           NULL,
+    [SearchCount] INT           DEFAULT ((0)) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
+
+
 
 
 
