b1b3e60f25522d6b9caf2134a30e7231f73b6e59
NshanKepityan <nshan757@gmail.com>
2018-07-30 20:19:05 +0400

    seller's online shop

--- WebClient/Controllers/ShopController.cs
+++ WebClient/Controllers/ShopController.cs
@@ -18,6 +18,7 @@ namespace WebClient.Controllers
 {
     public class ShopController : Controller
     {
+        //List<ProductModel> currentProducts = new List<ProductModel>();
         // GET: /<controller>/
       /*  public IActionResult Index()
         {
@@ -56,21 +57,21 @@ namespace WebClient.Controllers
         public async Task<IActionResult> Index()
          {
              // ... Target page.
-             Uri idSiteUri = new Uri("http://localhost:5001/api/SellerProduct/1");
+             Uri idSiteUri = new Uri("http://localhost:5003/api/SellerProduct/products/1");
              
-             List<KeyValuePair<string, int>> product_ids = new List<KeyValuePair<string, int>>();
 
              // ... Use HttpClient.
              using (HttpClient client = new HttpClient())
              {
                  using (HttpResponseMessage response = await client.GetAsync(idSiteUri))
-                 {
+                    {
                      using (HttpContent content = response.Content)
                      {
                         // ... Read the string.
                         string result = await content.ReadAsStringAsync();
-                        product_ids = JsonConvert.DeserializeObject<List<KeyValuePair<string, int>>>(result);
-                        if(product_ids == null)
+                        var products = JsonConvert.DeserializeObject<List<ProductModel>>(result);
+                        //this.currentProducts = products;
+                        /*if(product_ids == null)
                             Response.StatusCode = 500;
                         string prodSiteUri = "http://localhost:5002/api/Products/";
                         foreach(var prod in product_ids)
@@ -88,13 +89,66 @@ namespace WebClient.Controllers
                                 {
                                     Console.WriteLine(result.Substring(0, 50) + "...");
                                 }
-                                return View(products);
                             }
-                        }
-                        
-                     }
-                 }
+                        }*/
+                        return View(products);
+
+                    }
+                }
              }
          }
+        public async Task<IActionResult> Post(ProductModel instance)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UpdateViewAsync(int id)
+        {
+            // ... Target page.
+            Uri siteUri = new Uri("http://localhost:5002/api/Products/"+id);
+            var product = new ProductModel();
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(siteUri))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        // ... Read the string.
+                        string result = await content.ReadAsStringAsync();
+                        product = JsonConvert.DeserializeObject<ProductModel>(result);
+
+                        if (result != null &&
+                            result.Length >= 50)
+                        {
+                            Console.WriteLine(result.Substring(0, 50) + "...");
+                        }
+                    }
+                }
+            }
+            return View(product);
+        }
+
+        public async Task<IActionResult> Update(ProductModel instance)
+        {
+            // ... Target page.
+            Uri siteUri = new Uri("http://localhost:5002/api/Products/"+instance.Id);
+            CustomerModel customer = new CustomerModel();
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            {
+                var content = JsonConvert.SerializeObject(instance);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                using (HttpResponseMessage response = await client.PostAsync(
+                siteUri, byteContent))
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var responseStr = JsonConvert.DeserializeObject<String>(responseString);
+                    return RedirectToAction("Index", "Shop", new { msg = responseStr });
+                }
+            }
+        }
     }   
 }
--- WebClient/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
+++ WebClient/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
@@ -27,11 +27,11 @@
         return value.replace(/([!"#$%&'()*+,./:;<=>?@\[\\\]^`{|}~])/g, "\\$1");
     }
 
-    function getModelPrefix(fieldName) {
+    function getproductPrefix(fieldName) {
         return fieldName.substr(0, fieldName.lastIndexOf(".") + 1);
     }
 
-    function appendModelPrefix(value, prefix) {
+    function appendproductPrefix(value, prefix) {
         if (value.indexOf("*.") === 0) {
             value = value.replace("*.", prefix);
         }
@@ -360,9 +360,9 @@
     adapters.addMinMax("length", "minlength", "maxlength", "rangelength").addMinMax("range", "min", "max", "range");
     adapters.addMinMax("minlength", "minlength").addMinMax("maxlength", "minlength", "maxlength");
     adapters.add("equalto", ["other"], function (options) {
-        var prefix = getModelPrefix(options.element.name),
+        var prefix = getproductPrefix(options.element.name),
             other = options.params.other,
-            fullOtherName = appendModelPrefix(other, prefix),
+            fullOtherName = appendproductPrefix(other, prefix),
             element = $(options.form).find(":input").filter("[name='" + escapeAttributeValue(fullOtherName) + "']")[0];
 
         setValidationValues(options, "equalTo", element);
@@ -379,10 +379,10 @@
             type: options.params.type || "GET",
             data: {}
         },
-            prefix = getModelPrefix(options.element.name);
+            prefix = getproductPrefix(options.element.name);
 
         $.each(splitAndTrim(options.params.additionalfields || options.element.name), function (i, fieldName) {
-            var paramName = appendModelPrefix(fieldName, prefix);
+            var paramName = appendproductPrefix(fieldName, prefix);
             value.data[paramName] = function () {
                 var field = $(options.form).find(":input").filter("[name='" + escapeAttributeValue(paramName) + "']");
                 // For checkboxes and radio buttons, only pick up values from checked fields.
