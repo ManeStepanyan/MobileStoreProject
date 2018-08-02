96fb4cac2deaa8865f8db0a1417a54efd15cd8e2
manestep <manestep@gmail.com>
2018-08-02 14:07:33 +0400

    padding-top added

--- WebClient/Controllers/HomeController.cs
+++ WebClient/Controllers/HomeController.cs
@@ -38,6 +38,10 @@ namespace WebClient.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public async Task<IActionResult> ShopCart()
+        {
+            return Ok();
+        }
 
         public async Task<IActionResult> ProductsAsync()
         {
