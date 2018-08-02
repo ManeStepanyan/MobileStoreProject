eac34c902815ca88834ddaed9df9a0f001dda23c
Lina1999 <evelineghazaryan@gmail.com>
2018-08-03 01:15:09 +0400

    search position fixed, admins table fk correctded

--- UsersDatabase/Tables/Admins.sql
+++ UsersDatabase/Tables/Admins.sql
@@ -3,7 +3,7 @@
     [UserId] INT        NOT NULL,
     [Name]   NCHAR (30) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Admins_ToTableRoles] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+    CONSTRAINT [FK_Admins_ToTableUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
 );
 
 
