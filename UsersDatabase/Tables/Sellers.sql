54c2d0dd06aadbdf3453f97843a29d9ec354d8ea
manestep <manestep@gmail.com>
2018-08-11 14:26:21 +0400

    5 stars :D

--- UsersDatabase/Tables/Sellers.sql
+++ UsersDatabase/Tables/Sellers.sql
@@ -4,10 +4,12 @@
     [Name]      VARCHAR (30) NOT NULL,
     [CellPhone] VARCHAR (30) NOT NULL,
     [Address]   VARCHAR (60) NOT NULL,
-    [Rating]    FLOAT (53)   CONSTRAINT [df_ConstraintNAme] DEFAULT ((10)) NULL,
+    [Rating]    FLOAT (53)   CONSTRAINT [df_ConstraintNAme] DEFAULT ((5)) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Sellers_ToTableUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
     UNIQUE NONCLUSTERED ([Address] ASC)
 );
+
+
 
 
